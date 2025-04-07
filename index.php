c203f0699d86b9133b0180cf3a91f1373b80ba50
Arno Pan <apan11@my.bcit.ca>
2025-04-07 15:07:45 -0700

    added some styles

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
 
-if(isset($_GET["operation"])){
+if (isset($_GET["operation"])) {
     $num1 = $_GET["number1"];
     $num2 = $_GET["number2"];
     $operand = $_GET["operation"];
     $msg = "";
 
-    if(!is_numeric($num1) || !is_numeric($num2)){
+    if (!is_numeric($num1) || !is_numeric($num2)) {
         $msg = "Please enter numbers.";
-    }else{
-        switch($operand){
-            case "+" : 
+    } else {
+        switch ($operand) {
+            case "+":
                 $result = $num1 + $num2;
                 break;
-            case "-" : 
+            case "-":
                 $result = $num1 - $num2;
                 break;
-            case "x" : 
+            case "x":
                 $result = $num1 * $num2;
                 break;
-            case "/" : 
-                if($num2 != 0){
+            case "/":
+                if ($num2 != 0) {
                     $result = $num1 / $num2;
-                }else{
+                } else {
                     $result = 0;
                 }
                 break;
@@ -34,38 +34,45 @@ if(isset($_GET["operation"])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Calculator</title>
 </head>
+
 <body>
     <h1>PHP Calculator</h1>
-    <form action="<?= $_SERVER['PHP_SELF']?>" method="get">
-        <!-- First Number -->
-        <div>
-            <label for="number1">First Number</label>
-            <input id="number1" value="<?= $num1 ?>" name="number1" type="number"  />
-        </div>
-        <!-- Second Number -->
-        <div>
-            <label for="number2">Second Number</label>
-            <input id="number2" value="<?= $num2 ?>" name="number2" type="number"  />
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+        <div id="containerOperands">
+            <!-- First Number -->
+            <div id="container1">
+                <label id='label1' for="number1">Operand A: </label>
+                <input id="number1" value="<?= $num1 ?>" name="number1" type="number" />
+            </div>
+            <!-- Second Number -->
+            <div id=container2>
+                <label id='label2' for="number2">Operand B: </label>
+                <input id="number2" value="<?= $num2 ?>" name="number2" type="number" />
+            </div>
         </div>
         <!-- Result Number -->
-        <div>
-            <label for="result">Result</label>
-            <input id="result" value="<?= $result ?>" name="result" type="number" disabled  />
+        <div id="containerResult">
+            <label for="result">Result: </label>
+            <input id="result" value="<?= $result ?>" name="result" type="number" disabled />
         </div>
         <!-- Operation -->
-        <div>
+        <div id="containerOperations">
+            <label for="operation">Operation</label>
             <input type="submit" id="sum" value="+" name="operation">
             <input type="submit" id="subtract" value="-" name="operation">
             <input type="submit" id="multiply" value="x" name="operation">
             <input type="submit" id="divide" value="/" name="operation">
         </div>
     </form>
-    <p><?= $msg ?></p>
+    <p id="message"><?= $msg ?></p>
 </body>
+
 </html>
